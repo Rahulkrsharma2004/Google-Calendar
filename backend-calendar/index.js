afe392e475f866a9c4cb28beafd47f7e699fb446
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2024-11-17 23:21:05 +0530

    cors configration

--- backend-calendar/index.js
+++ backend-calendar/index.js
@@ -7,7 +7,10 @@ const eventRoutes = require('./routes/eventRoutes');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://google-calendar-rks.vercel.app', 
+  credentials: true,
+}));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
